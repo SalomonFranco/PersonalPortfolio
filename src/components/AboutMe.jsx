255f4119e1eca029acc4a8698ba07be3d782c913
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2025-04-09 16:18:45 +0000

    aboutMeUpdate

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -38,53 +38,7 @@ const AboutMe = ({ name }) => {
 
   return (
     <section className="about">
-      <div className="aboutContainer container">
-        <div className="row">
-          <motion.div
-            className="personalImage col-12 col-lg-6"
-            ref={ref}
-            initial={{ x: "-10vw", opacity: 0, scale: 0.5 }}
-            animate={inView ? { x: 0, opacity: 1, scale: 1 } : { x: "-10vw", opacity: 0, scale: 0.5 }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-            whileHover={{ scale: 1.05 }}
-          >
-            {/* Display the personal image */}
-            <motion.img src={aboutMeImg} alt={name} />
-          </motion.div>
-          <div className="personalInfo col-12 col-lg-6">
-            <motion.div className="contentContainer" variants={staggerVariants}>
-              {/* Display greeting and job title with animation */}
-              <motion.h4 variants={paragraphVariants}>Pleased to meet you! 👋🏻</motion.h4>
-              <motion.h5 variants={paragraphVariants}>I'm Salomon Franco, a Software Developer.</motion.h5>
-
-              {/* Display content description with animation */}
-              <motion.div
-                className="contentDescription"
-                variants={staggerVariants}
-                initial="initial"
-                animate={inView ? "animate" : "initial"}
-              >
-                {/* Paragraphs with animation */}
-                <motion.p variants={paragraphVariants}>
-                My journey into coding has been driven by a passion for technology and curiosity. From an early age, 
-                I've enjoyed dismantling gadgets to understand them better.
-                </motion.p>
-                <br />
-                <motion.p variants={paragraphVariants}>
-                Coding isn't just a career; it's my true passion. Crafting elegant solutions to complex problems, 
-                whether designing user-friendly interfaces or optimizing back-end algorithms, brings me immense joy. 
-                I'm constantly learning and exploring new technologies.
-                </motion.p>
-              </motion.div>
-
-              {/* Button to view the portfolio */}
-              <NavLink to="/portfolio">
-                <Button name="View Portfolio" />
-              </NavLink>
-            </motion.div>
-          </div>
-        </div>
-      </div>
+      
     </section>
   );
 };
